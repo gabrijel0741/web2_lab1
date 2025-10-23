1e944406457fc5b866d597ac802546910a390111
gabrijel0741 <gabrijel0741@gmail.com>
2025-10-23 14:21:03 +0200

    idk

--- routes/home.routes.js
+++ routes/home.routes.js
@@ -3,8 +3,7 @@ const router = express.Router();
 
 router.get('/', async function (req, res, next) {
     res.render('home', {
-        title: 'Home',
-        user: req.session.user,
+        title: 'Home'
     });
 });
 
