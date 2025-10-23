53d8b8bbd11adad49e4eebcca445df768a0a7761
gabrijel0741 <gabrijel0741@gmail.com>
2025-10-23 14:35:20 +0200

    session fill db

---
+++ db/seed.js
@@ -0,0 +1,92 @@
+const {Pool} = require('pg');
+
+const pool = new Pool({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: 'projektrpetersen',
+    password: process.env.DB_PASSWORD,
+    port: 5432,
+    ssl: { rejectUnauthorized: false }
+});
+
+const sql_create_users = `CREATE TABLE users (
+    user_id SERIAL PRIMARY KEY,
+    user_name text NOT NULL UNIQUE,
+    first_name text NOT NULL,
+    last_name text NOT NULL,
+    password text NOT NULL
+)`;
+
+
+const sql_create_sessions = `CREATE TABLE session (
+    sid varchar NOT NULL COLLATE "default",
+    sess json NOT NULL,
+    expire timestamp(6) NOT NULL
+  )
+  WITH (OIDS=FALSE);`
+
+const sql_create_session_index1 = `ALTER TABLE session ADD CONSTRAINT session_pkey PRIMARY KEY (sid) NOT DEFERRABLE INITIALLY IMMEDIATE`
+const sql_create_session_index2 = `CREATE INDEX IDX_session_expire ON session(expire)`
+
+const sql_insert_users = `INSERT INTO users (user_name, first_name, last_name, password) VALUES ('aadmin', 'admin', 'adminko', 'aadmin'),
+                          ('testiranje', 'test', 'testić', 'testiranje')`
+
+let table_names = [
+    "users",
+    "session"
+]
+
+let tables = [
+    sql_create_users,
+    sql_create_sessions
+];
+
+let table_data = [
+    sql_insert_users,
+    undefined
+]
+
+let indexes = [
+  sql_create_session_index1,
+  sql_create_session_index2
+]
+
+if ((tables.length !== table_data.length) || (tables.length !== table_names.length)) {
+    console.log("tables, names and data arrays length mismatch.")
+    
+}
+
+(async () => {
+    console.log("Creating and populating tables");
+    for (let i = 0; i < tables.length; i++) {
+        console.log("Creating table " + table_names[i] + ".");
+        try {
+            await pool.query(tables[i], [])
+            console.log("Table " + table_names[i] + " created.");
+            if (table_data[i] !== undefined) {
+                try {
+                    await pool.query(table_data[i], [])
+                    console.log("Table " + table_names[i] + " populated with data.");
+                } catch (err) {
+                    console.log("Error populating table " + table_names[i] + " with data.")
+                    return console.log(err.message);
+                }
+            }
+        } catch (err) {
+            console.log("Error creating table " + table_names[i])
+            return console.log(err.message);
+        }
+    }
+
+    console.log("Creating indexes");
+    for (let i = 0; i < indexes.length; i++) {
+        try {
+            await pool.query(indexes[i], [])
+            console.log("Index " + i + " created.")
+        } catch (err) {
+            console.log("Error creating index " + i + ".")
+        }
+    }
+
+    await pool.end();
+})()
