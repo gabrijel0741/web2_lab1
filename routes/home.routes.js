4dfd5e303f39c939c9f744c4f51142c3abeec81d
gabrijel0741 <gabrijel0741@gmail.com>
2025-10-23 13:48:33 +0200

    home route fix

--- routes/home.routes.js
+++ routes/home.routes.js
@@ -1,13 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const Quiz = require('../models/QuizModel');
 
 router.get('/', async function (req, res, next) {
-    let leaderboard = await Quiz.fetchLeaderboard();
     res.render('home', {
         title: 'Home',
         user: req.session.user,
-        leaderboard: leaderboard
     });
 });
 
