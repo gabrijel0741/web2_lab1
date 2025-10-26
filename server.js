332fc6fbd54d440e6ff18b50d6a59ab9f1faf613
gabrijel0741 <gabrijel0741@gmail.com>
2025-10-26 18:43:56 +0100

    ti

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.set('view engine', 'ejs');
 const oidcConfig = {
   authRequired: false, 
   auth0Logout: true,
-  secret: process.env.AUTH0_SECRET,
+  secret: process.env.AUTH0_CLIENT_SECRET,
   baseURL: process.env.AUTH0_BASE_URL,
   clientID: process.env.AUTH0_CLIENT_ID,
   issuerBaseURL: process.env.AUTH0_ISSUER_BASE_URL
