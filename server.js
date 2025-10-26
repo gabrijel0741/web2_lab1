0f7d37c8de9171ee1b9e084c0a3269297cc41884
gabrijel0741 <gabrijel0741@gmail.com>
2025-10-26 16:49:33 +0100

    ae

--- server.js
+++ server.js
@@ -23,8 +23,7 @@ const oidcConfig = {
   secret: process.env.AUTH0_SECRET,
   baseURL: process.env.AUTH0_BASE_URL,
   clientID: process.env.AUTH0_CLIENT_ID,
-  issuerBaseURL: process.env.AUTH0_ISSUER_BASE_URL,
-  audience: process.env.AUTH0_AUDIENCE
+  issuerBaseURL: process.env.AUTH0_ISSUER_BASE_URL
 };
 
 app.use(authOIDC(oidcConfig));
